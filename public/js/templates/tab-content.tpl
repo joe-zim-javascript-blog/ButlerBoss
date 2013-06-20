e47197b758292c4c3f7132fd3fd2160702813945
Joseph Zimmerman <contact@joezimjs.com>
2013-06-20 00:59:04 -0500

    Converted TabContentView to be a wrapper around other views instead of an extendable view

---
+++ public/js/templates/tab-content.tpl
@@ -0,0 +1,1 @@
+<div class="App-TabContentContainer"></div>
--- public/js/views/tab-content.js
+++ public/js/views/tab-content.js
@@ -2,18 +2,13 @@ define(
 	['marionette', 'tpl!templates/tab-content.tpl'],
 	function(Marionette, template) {
 
-		var activeClass = "active";
-
-		var TabContentView = Marionette.ItemView.extend({
+		var TabContentView = Marionette.Layout.extend({
 			tagName: 'div',
 			className: 'tab-pane',
+			template: template,
 
-			activate: function() {
-				this.$el.addClass(activeClass);
-			},
-
-			deactivate: function() {
-				this.$el.removeClass(activeClass);
+			regions: {
+				content: '.App-TabContentContainer'
 			}
 		});
 
