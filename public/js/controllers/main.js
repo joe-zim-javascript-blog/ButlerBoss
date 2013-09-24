b9acc4cb9c9da276212b0ffa58488b72789a6795
Joseph Zimmerman <contact@joezimjs.com>
2013-09-24 10:34:09 -0500

    Small changed to the way bootstrapping the app is working. Also a minor change to the index.html file that I forgot to commit earlier.

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -1,26 +1,12 @@
 define(
-	['marionette', 'underscore', 'models/servers', 'utils/tab-manager'],
-	function(Marionette, _, Servers, TabManager) {
+	['marionette', 'underscore', 'utils/tab-manager'],
+	function(Marionette, _, TabManager) {
 
 		var MainController = Marionette.Controller.extend({
 
 			initialize: function(options) {
 				this.options = options || {};
-
-				this.options.mainContainer = this.options.mainContainer || "[data-view-binding=layout]";
 				this.vent = this.options.vent;
-
-				this.initializeData();
-			},
-
-			initializeData: function() {
-				if ( this.options.minecraftServerData ) {
-					App.servers = new Servers(this.options.minecraftServerData);
-				}
-				else {
-					App.servers = new Servers();
-					App.servers.fetch();
-				}
 			},
 
 			index: function() {
